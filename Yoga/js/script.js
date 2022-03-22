f229324615010ccf0ead6923e1126b1e624b2621
Anton Karabasov <antonkarabasov22@gmail.com>
2022-03-22 17:17:05 +0300

    Added submit form and installed babel/polyfill in Yoga

--- Yoga/js/script.js
+++ Yoga/js/script.js
@@ -38,13 +38,13 @@ window.addEventListener('DOMContentLoaded', function() {
 	});
 
 	//Таймер
-	let deadline = '2022-03-22';
+	let deadline = '2022-03-25';
 
 	function getTimeRemaining(endtime) {
 		let t = Date.parse(endtime) - Date.parse(new Date()),
 		seconds = Math.floor((t/1000) % 60),
 		minutes = Math.floor((t/1000/60) % 60),
-		hours = Math.floor((t/1000/60/60) % 24);
+		hours = Math.floor((t/1000/60/60));
 
 		return {
 			'total' : t,
@@ -103,5 +103,52 @@ window.addEventListener('DOMContentLoaded', function() {
 		overlay.style.display = 'none';
 		more.classList.remove('more-splash');
 		document.body.style.overflow = '';
+	});
+
+	//Форма 
+	let message = {
+		loading: 'Загрузка...',
+		succes: 'Спасибо! Скоро мы с вами свяжемся',
+		failure: 'Что-то пошло не так...'
+	};
+
+	let form = document.querySelector('.main-form'),
+		input = form.getElementsByTagName('input'),
+		statusMessage = document.createElement('div');
+
+	statusMessage.classList.add('status');
+
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
+		form.appendChild(statusMessage);
+
+		let request =  new XMLHttpRequest;
+		request.open('POST', 'server.php');
+		//request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+		let formData = new FormData(form);
+
+		let obj = {};
+		formData.forEach(function(value, key) {
+			obj[key] = value;
+		});
+		let json = JSON.stringify(obj);
+
+		request.send(json);
+
+		request.addEventListener('readystatechange', function() {
+			if (request.readyState < 4) {
+				statusMessage.innerHTML = message.loading;
+			} else if (request.readyState === 4 && request.status == 200) {
+				statusMessage.innerHTML = message.succes;
+			} else {
+				statusMessage.innerHTML = message.failure;
+			}
+		});
+
+		for (let i = 0; i < input.length; i++) {
+			input[i].value = '';
+		}
 	});
 });
