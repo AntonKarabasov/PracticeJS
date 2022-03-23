71b7673b9d0db7a86e82361cc393ad00e5c74c7f
Anton Karabasov <antonkarabasov22@gmail.com>
2022-03-23 12:02:02 +0300

    Update send form with Promise

--- Yoga/js/script.js
+++ Yoga/js/script.js
@@ -122,33 +122,46 @@ window.addEventListener('DOMContentLoaded', function() {
 		event.preventDefault();
 		form.appendChild(statusMessage);
 
-		let request =  new XMLHttpRequest;
-		request.open('POST', 'server.php');
-		//request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-		request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-
 		let formData = new FormData(form);
+		
+		function postData(data) {
+			return new Promise(function(resolve, reject) {
+				let request =  new XMLHttpRequest;
+				request.open('POST', 'server.php');
+				//request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+				request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
 
-		let obj = {};
-		formData.forEach(function(value, key) {
-			obj[key] = value;
-		});
-		let json = JSON.stringify(obj);
+				let obj = {};
+				data.forEach(function(value, key) {
+					obj[key] = value;
+				});
+				let json = JSON.stringify(obj);
 
-		request.send(json);
+				request.onreadystatechange = function() {
+					if (request.readyState < 4) {
+						resolve();
+					} else if (request.readyState === 4 && request.status == 200) {
+						resolve();
+					} else {
+						reject();
+					}
+				};
 
-		request.addEventListener('readystatechange', function() {
-			if (request.readyState < 4) {
-				statusMessage.innerHTML = message.loading;
-			} else if (request.readyState === 4 && request.status == 200) {
-				statusMessage.innerHTML = message.succes;
-			} else {
-				statusMessage.innerHTML = message.failure;
-			}
-		});
-
-		for (let i = 0; i < input.length; i++) {
-			input[i].value = '';
+				request.send(json);
+			});
 		}
+		
+
+		function claerInput() {
+			for (let i = 0; i < input.length; i++) {
+				input[i].value = '';
+			}
+		}
+
+		postData(formData)
+			.then(() => statusMessage.innerHTML = message.loading)
+			.then(() => statusMessage.innerHTML = message.succes)
+			.catch(() => statusMessage.innerHTML = message.failure)
+			.then(claerInput);
 	});
 });
